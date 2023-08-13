20b338e3d8587726dc655c806449cd9c92efcda7
Marcus Green <marcusavgreen@gmail.com>
2023-08-13 10:53:00 +0100

    Create spec.cy.js

---
+++ spec.cy.js
@@ -0,0 +1,28 @@
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('/login/index.php')
+  })
+  it('is true', () => {
+
+  cy.get('#username')
+  .type('admin');
+
+  cy.get('#password')
+  .type('password');
+  cy.get('#loginbtn').click();
+  cy.visit('https://master.localhost/course/management.php');
+
+
+  if(cy.contains('Cypress test course 101')) {
+    pause();
+  }
+  cy.get('a').contains('Create new course').click();
+  cy.get('#id_fullname')
+  .type('Cypress test course 101');
+  cy.get('#id_shortname')
+  .type('CYTC101');
+  cy.get('#id_saveanddisplay').click();
+  cy.get('button').contains('More').click();
+})
+
+})
