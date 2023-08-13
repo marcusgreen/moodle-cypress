880e99b1ea3dee140b04af33b13117ca5fbd135a
Marcus Green <marcusavgreen@gmail.com>
2023-08-13 10:51:56 +0100

    Create cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    experimentalStudio: true,
+    baseUrl: 'http://master.localhost',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
