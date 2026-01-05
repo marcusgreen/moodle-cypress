cd607ee4d3e953b895b88a70bd2b47a8fe77349d
Marcus Green <marcusavgreen@gmail.com>
2026-01-05 19:04:46 +0000

    Rearrange files to match standard Cypress directory structure

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'http://p53/mdl45',
-    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
     env: {
       MOODLE_ADMIN_PASSWORD: process.env.CYPRESS_MOODLE_ADMIN_PASSWORD || 'Password1!'
